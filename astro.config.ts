754fe5620c9a1bc9bcffd2222615910c0cc08a9e
Proxy Alt <proxy-alt@proxy-alt.dev>
2026-01-01 13:28:06 -0500

    Push something x2

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
 
 export default defineConfig({
   output: 'server',
@@ -10,6 +11,20 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [
+    AstroPWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+    }),
+  ],
   experimental: {
     svgo: true
   }
